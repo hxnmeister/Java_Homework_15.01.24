5b035cd4ea555b6ffc6908a5c4242f498d1dae53
Valerii <valeriy.stronskiy@gmail.com>
2024-01-16 09:02:29 +0200

    Minor changes to Task #1

--- src/Task1.java
+++ src/Task1.java
@@ -1,6 +1,6 @@
 public class Task1 {
-    public static void main(String args[]){
-        String phrase[] = {"\"Your time is limited,", "so don`t waste it", "living someone else`s life\" ", "Steve Jobs"};
+    public static void main(String[] args){
+        String[] phrase = {"\"Your time is limited,", "so don`t waste it", "living someone else`s life\" ", "Steve Jobs"};
         String indent = "";
 
         for (String s : phrase) {
